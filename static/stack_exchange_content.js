a5780244db515235cc68b648a41e19cea58808e0
Matthew Casperson <matthewcasperson@gmail.com>
2013-04-04 11:20:39 +1000

    Working on updating tree

--- static/stack_exchange_content.js
+++ static/stack_exchange_content.js
@@ -1,10 +1,16 @@
-/** A collection of content items ready to be added to the book */
-stagingContent = [];
-
 function addToStage() {
 	var stageObject = {};
 	
-	var url = jQuery('#questionUrl').text();
+	var url = jQuery('#questionUrl').val();
 	
-	var regex = XRegExp('(http://)|(https://)(?<hostname>.*?).com/questions/(?<questionId>\\d+)');
+	var regex = XRegExp('(http://|https://)(?<hostname>.*?).com/questions/(?<questionId>\\d+)');
+	var parts = XRegExp.exec(url, regex);
+	
+	if (parts.hostname !== undefined && parts.questionId !== undefined) {
+		stageObject.site =  parts.hostname;
+		stageObject.question = parts.questionId;
+
+		var newNode = $("#toc-staging").jstree("create",-1,false,stageObject.site + " " + stageObject.question,false,true)[0];
+		newNode.data = stageObject;
+	}
 }
