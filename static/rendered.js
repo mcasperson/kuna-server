ead5360f3039a03a5fa0b16638e6aef157ae4a50
Matthew Casperson <matthewcasperson@gmail.com>
2013-04-04 12:53:08 +1000

    Working on updating tree

--- static/rendered.js
+++ static/rendered.js
@@ -9,7 +9,33 @@ function renderedElement(child) {
 		var div = jQuery('<div><h3>' + child.name + '</h3><div>');
 		jQuery('#rendered').append(div);
 	} else if (child.type == BookElementEmun.SEContent) {
-		var div = jQuery('<div>' + child.seData.body + '<div>');
+		var text = child.seData.body;
+		if (child.seData.answers !== undefined) {
+			var accepted_answer_id = child.seData.accepted_answer_id;
+			
+			if (accepted_answer_id === undefined) {
+				var maxScore = undefined;
+				jQuery.each(child.seData.answers, function(i, answer){
+					if (maxScore === undefined || answer.score > maxScore) {
+						text = answer.body;
+						maxScore = answer.score;
+					}
+				});	
+			} else {
+				jQuery.each(child.seData.answers, function(i, answer){
+					if (answer.answer_id == accepted_answer_id) {
+						text = answer.body;
+						return false;
+					}
+				});	
+			}
+		}
+		
+		var div = jQuery(
+			'<div>' +
+				'<h4>' + child.name + '</h4>' +
+				'<div>' + text + '</div>' +
+			'<div>');
 		jQuery('#rendered').append(div);
 	}
 	
